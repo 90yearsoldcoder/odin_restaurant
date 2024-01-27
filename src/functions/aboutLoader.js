2b9ff78b191b2483aec0eeb6498aedaf341eb901
90yearsoldcoder <minty@bu.edu>
2024-01-27 14:42:34 -0500

    re-enable git page

--- src/functions/aboutLoader.js
+++ src/functions/aboutLoader.js
@@ -1,7 +1,7 @@
 import "../styles/about.css";
 
 const about_load = function () {
-  alert("about page: blank");
+  alert("about page: blank ");
 
   const aboutus_item = content.querySelector("#Aboutus");
   aboutus_item.classList.add("nav-active");
